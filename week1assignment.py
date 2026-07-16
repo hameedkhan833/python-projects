5e42e05bae11b8211516b10f0425abdb9fc7eed5
hameedkhan833 <hameedprokhan92@gmail.com>
2026-07-16 05:33:17 -0600

    Add files via upload

---
+++ week1assignment.py
@@ -0,0 +1,13 @@
+# ---------------Basic Calculator---------------
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+operation = input("Enter operation(+, -, *, /) : ")
+
+if operation == "+":
+    print("Addition : ", num1 + num2)
+elif operation == "-":
+    print("Subtraction : ", num1 - num2)
+elif operation == "*":
+    print("Multiplication : ", num1 * num2)
+elif operation == "/":
+    print("Division : ", num1 / num2)            
